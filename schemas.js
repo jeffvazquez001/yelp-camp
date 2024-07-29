c7b3fa0ee9452cd266609ef0cf31c35998cce3d2
Jeff Vazquez <jeffvazquez@cpp.edu>
2024-07-29 15:09:28 -0700

    fixing sanitize html

--- schemas.js
+++ schemas.js
@@ -1,5 +1,5 @@
 const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
+const sanitizeHTML = require('sanitize-html');
 const extension = (joi) => ({
     type: 'string',
     base: joi.string(),
