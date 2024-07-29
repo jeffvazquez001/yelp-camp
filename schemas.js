2e4c768dbf3009ce5fd9197c7cff7e15df0de29a
Jeff Vazquez <jeffvazquez@cpp.edu>
2024-07-29 14:45:30 -0700

    fixed sanitize html

--- schemas.js
+++ schemas.js
@@ -4,7 +4,7 @@ const extension = (joi) => ({
     type: 'string',
     base: joi.string(),
     messages: {
-        'string.escapeHTML' : '{{#label}} must not include HTML'
+        'string.escapeHTML': '{{#label}} must not include HTML!'
     },
     rules : {
         escapeHTML: {
@@ -13,7 +13,7 @@ const extension = (joi) => ({
                     allowedTags: [],
                     allowedAttributes: {},
                 });
-                if(clean !== value) return helpers.error('string.escapreHTML', {value})
+                if(clean !== value) return helpers.error('string.escapeHTML', {value})
                     return clean;
             }
         }
