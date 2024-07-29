eae1d84e155509cc05edb73f8a4b79b2e72f88a5
Jeff Vazquez <jeffvazquez@cpp.edu>
2024-07-29 15:21:05 -0700

    fixing function for maptilerClient

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -14,7 +14,7 @@ module.exports.renderNewForm = (req,res) => {
 }
 
 module.exports.createCampground = async(req,res) =>{
-    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 }).send();
+    const geoData = await maptilerClient.geocoding.forward(req.body.campground.location, { limit: 1 });
     const campground = new Campground(req.body.campground);
     campground.geometry = geoData.features[0].geometry;
     campground.images =  req.files.map(f=>({url: f.path, filename: f.filename}))
